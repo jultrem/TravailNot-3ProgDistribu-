24d03f818f958238e73906485bb596bc254bbc9f
ulrlaro <ulrlaro@users.noreply.github.com>
2022-11-08 17:13:11 -0500

    python-javascript

--- Paie/paie.js
+++ Paie/paie.js
@@ -1,1 +1,53 @@
-console.log("Hello World");
+const express = require('express')
+const mariadb = require('mariadb');
+const app = express()
+const port = 3000
+const pool = mariadb.createPool({
+    host: 'host.docker.internal', 
+    user:'root', 
+    password: 'toor',
+    database: 'fleuron'
+});
+
+app.get('/paye/talons', async (req, res) => {
+  if (req.query.employe)
+    row = await listerTalonsParEmploye(req.query.employe)
+  else
+  {
+    row = await listerTalons()
+  }
+  res.send(row)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+async function listerTalons() {
+  let conn;
+  try {
+	  conn = await pool.getConnection();
+    list = {}
+	  const rows = await conn.query("SELECT TalonID, EmployeID, Contenu FROM talons");
+    conn.end()
+	  return rows 
+  } catch (err) {
+    console.log(err)
+	throw err
+  }
+}
+
+async function listerTalonsParEmploye(employeId) {
+    let conn; 
+    try {
+      conn = await pool.getConnection();
+      list = {};
+      const rows = await conn.query("SELECT TalonID, EmployeID, Contenu FROM talons WHERE EmployeID=?",[employeId])
+      conn.end()
+      return rows
+    } catch (err) {
+      console.log(err)
+      throw err
+    }
+  }
---
+++ Production/main.py
@@ -0,0 +1,60 @@
+from typing import List
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+import mariadb
+
+import sys
+
+#app = FastAPI(openapi_url=None)
+
+# Connect to MariaDB Platform
+try:
+    conn = mariadb.connect(
+        user="root",
+        password="toor",
+        host="host.docker.internal",
+        #host="localhost",
+        port=3306,
+        database="fleuron"
+    )
+except mariadb.Error as e:
+    print(f"Zut, Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
+
+# Get Cursor
+cursor = conn.cursor()
+
+app = FastAPI(root_path="/production/")
+
+
+class SiteBD(BaseModel):
+    id: int
+    nom_site: str
+
+class projetBD(BaseModel):
+    projet_id: int
+    nom_site: str
+    site_id: int
+
+@app.get("/production/")
+def root():
+    return {"message": "Bienvenue sur Fleuron insdustrie"}
+
+@app.get("/production/sites", response_model=List[SiteBD])
+def recuperer_sites():
+    cursor.execute("SELECT SiteID, Nom FROM sites")
+    tuples_projet = cursor.fetchall()
+    projets = [SiteBD(id=t[0], nom_site=t[1]) for t in tuples_projet]
+    return projets
+
+
+@app.get("/production/sites/{id_site}/projets", response_model=List[projetBD])
+def recuperer_projes_en_fonction_de_site(id_site: int):
+    cursor.execute("SELECT SiteID, Nom, ProjetID FROM projets WHERE SiteID = ?", (id_site,))
+    tuples_projet = cursor.fetchall()               
+    if tuples_projet is None:
+        raise HTTPException(status_code=404, detail=f"Une site ayant l'identifiant {id_site} n'a pu être trouvée")
+    projets = [projetBD(projet_id=t[2], nom_site=t[1], site_id=t[0]) for t in tuples_projet]
+    return projets
